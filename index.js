d7539c04fd8ec48412a351b138ee4d2f66d470b1
Niall Cunningham <hello@niallcunningham.com>
2018-12-29 14:13:33 +0000

    test

---
+++ index.js
@@ -0,0 +1,9 @@
+function toggleNav() {
+  var x = document.getElementById("dropdown-content")
+  if (x.style.display === "none") {
+    x.style.display === "block";
+  } else {
+    x.style.display === "none";
+  }
+}
+
